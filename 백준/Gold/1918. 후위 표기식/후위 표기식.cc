1a58193325083c2da7036d366ad08dde32d5a99c
Chaewon Song <chaewon1019@ewhain.net>
2024-02-26 23:26:27 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.cc
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -4,36 +4,31 @@
 
 using namespace std;
 
-// 우선순위 반환 함수
-int getPriority(char op) {
-    if (op == '*' || op == '/') return 2;
-    else if (op == '+' || op == '-') return 1;
-    else return 0; // 피연산자는 가장 낮은 우선순위
+int priority(char operators) {
+    if (operators == '*' || operators == '/') return 2;
+    else if (operators == '+' || operators == '-') return 1;
+    else return 0;
 }
 
-// 중위 표기식을 후위 표기식으로 변환하는 함수
 string infixToPostfix(const string& infix) {
     stack<char> operators;
     string postfix;
 
     for (char token : infix) {
-        // 피연산자는 그대로 출력
         if (isalnum(token)) {
             postfix += token;
         }
-        // 연산자 처리
         else if (token == '(') {
             operators.push(token);
         } else if (token == ')') {
-            // '('를 만날 때까지 스택에서 연산자를 pop하여 출력
             while (!operators.empty() && operators.top() != '(') {
                 postfix += operators.top();
                 operators.pop();
             }
-            operators.pop(); // '(' 제거
+            operators.pop(); 
         } else {
-            // 스택의 top에 있는 연산자의 우선순위가 더 크면 pop 후 출력
-            while (!operators.empty() && getPriority(operators.top()) >= getPriority(token)) {
+            // 스택의 탑에 있는 연산자의 우선순위가 더 크면 pop 후 출력
+            while (!operators.empty() && priority(operators.top()) >= priority(token)) {
                 postfix += operators.top();
                 operators.pop();
             }
@@ -41,8 +36,7 @@ string infixToPostfix(const string& infix) {
         }
     }
 
-    // 남아있는 연산자를 모두 출력
-    while (!operators.empty()) {
+    while (!operators.empty()) { // 남아있는 연산자를 모두 출력
         postfix += operators.top();
         operators.pop();
     }
