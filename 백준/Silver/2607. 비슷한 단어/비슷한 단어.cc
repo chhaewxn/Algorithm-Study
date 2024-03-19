dd747657059b961f3986d1a7e2df1afbde05f9d8
Chaewon Song <chaewon1019@ewhain.net>
2024-03-19 16:20:40 +0900

    [Silver II] Title: 비슷한 단어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.cc
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <set>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int countSimilarWords(const vector<string>& words) {
+int similar(const vector<string>& words) {
     int count = 0;
 
     multiset<char> baseSet(words[0].begin(), words[0].end()); // 첫 번째 단어의 문자로 이루어진 multiset 생성
@@ -12,11 +13,10 @@ int countSimilarWords(const vector<string>& words) {
     for (int i = 1; i < words.size(); ++i) {
         multiset<char> currentSet(words[i].begin(), words[i].end()); // i번째 단어의 문자로 이루어진 multiset 생성
 
-        multiset<char> diffSet1, diffSet2; // 두 단어 간의 차집합을 담을 multiset
+        multiset<char> diffSet1, diffSet2; 
         set_difference(baseSet.begin(), baseSet.end(), currentSet.begin(), currentSet.end(), inserter(diffSet1, diffSet1.begin()));
         set_difference(currentSet.begin(), currentSet.end(), baseSet.begin(), baseSet.end(), inserter(diffSet2, diffSet2.begin()));
 
-        // 두 단어 간의 차집합의 크기가 1 이하여야 비슷한 단어로 판별
         if (diffSet1.size() <= 1 && diffSet2.size() <= 1) {
             ++count;
         }
@@ -27,7 +27,7 @@ int countSimilarWords(const vector<string>& words) {
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0);
 
     int N;
     cin >> N;
@@ -37,7 +37,7 @@ int main() {
         cin >> words[i];
     }
 
-    int similarWords = countSimilarWords(words); // 비슷한 단어 개수 계산
+    int similarWords = similar(words); 
 
     cout << similarWords << '\n';
 
