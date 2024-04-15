2c9947540b82ce9170bea4731c9694679bca922b
Chaewon Song <chaewon1019@ewhain.net>
2024-04-16 01:29:20 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 488 ms, Memory: 149284 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,19 +1,20 @@
 import sys
-from collections import Counter
 
 # 입력 받기
-N, M = map(int, sys.stdin.readline().split())
-words = [sys.stdin.readline().rstrip() for _ in range(N)]
+n, m = map(int, input().split())
+words = [sys.stdin.readline().rstrip() for _ in range(n)]
 
-# M 글자 이상인 단어만 필터링
-words = [word for word in words if len(word) >= M]
+# 단어의 길이와 출현 빈도를 저장할 딕셔너리 생성
+word_counts = {}
 
-# 단어의 빈도수 계산
-word_counts = Counter(words)
+# 각 단어의 길이와 출현 빈도를 계산하여 딕셔너리에 저장
+for word in words:
+    if len(word) >= m:
+        word_counts[word] = word_counts.get(word, 0) + 1
 
-# 조건에 맞게 정렬: 빈도수 내림차순, 길이 내림차순, 알파벳 사전순
-sorted_words = sorted(word_counts.keys(), key=lambda x: (-word_counts[x], -len(x), x))
+# 정렬 기준에 맞게 정렬
+sorted_words = sorted(word_counts.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
 
-# 결과 출력
-for word in sorted_words:
+# 출력
+for word, count in sorted_words:
     print(word)
