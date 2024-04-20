dfdbde3c21d2a189efd5e925308a88c14d45f37d
Chaewon Song <chaewon1019@ewhain.net>
2024-04-20 23:27:04 +0900

    [Silver III] Title: 크로스 컨트리, Time: 136 ms, Memory: 110784 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,32 +1,32 @@
 import sys
 
-T = int(sys.stdin.readline().strip())
+input = int(sys.stdin.readline().strip())
 
-for _ in range(T):
-    N = int(sys.stdin.readline().strip())
-    rank = list(map(int, sys.stdin.readline().strip().split()))
-    result = {}
+for _ in range(input):
+  n = int(sys.stdin.readline().strip())
+  rank = list(map(int, sys.stdin.readline().split()))
+  result = {}
 
-    for data in rank:
-        result[data] = result.get(data, 0) + 1
+  for data in rank:
+    result[data] = result.get(data, 0) + 1
 
-    fifth_goal_idx = [0] * (max(result.keys()) + 1)
-    score_map = {}
-    temp_map = {}
-    score = 1
+  fifth_score = [0] * (max(result.keys()) + 1)
+  score_map = {}
+  score = 1
+  temp = {}
 
-    for element in rank:
-        if result[element] >= 6:
-            temp_map[element] = temp_map.get(element, 0) + 1
+  for element in rank:
+    if result[element] >= 6:
+      temp[element] = temp.get(element, 0) + 1
 
-            if temp_map[element] <= 4:
-                score_map[element] = score_map.get(element, 0) + score
+      if temp[element] <= 4:
+        score_map[element] = score_map.get(element, 0) + score
 
-            if temp_map[element] == 5:
-                fifth_goal_idx[element] = score
-            score += 1
+      if temp[element] == 5:
+        fifth_score[element] = score
+      score += 1
 
-    key_data = list(score_map.keys())
-    key_data.sort(key=lambda x: (score_map[x], fifth_goal_idx[x]))
+  key_data = list(score_map.keys())
+  key_data.sort(key=lambda x: (score_map[x], fifth_score[x]))
 
-    print(key_data[0])
+  print(key_data[0])
