dbafed26458c63a9c5d9698d4cb5e23af983f1a2
Chaewon Song <chaewon1019@ewhain.net>
2025-05-05 02:32:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    total = 0
+    
+    for absolute, sign in zip(absolutes, signs):
+        if sign:
+            total += absolute  
+        else:
+            total -= absolute 
+    
+    return total
