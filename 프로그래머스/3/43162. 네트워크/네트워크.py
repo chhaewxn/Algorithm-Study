b297fe5c3ab5f46dbcb85d920370173e2e063c2e
Chaewon Song <chaewon1019@ewhain.net>
2025-07-20 01:17:04 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,16 @@
+def solution(n, computers):
+    visited = [False] * n
+
+    def dfs(node):
+        visited[node] = True
+        for next_node in range(n):
+            if computers[node][next_node] == 1 and not visited[next_node]:
+                dfs(next_node)
+
+    count = 0
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            count += 1
+
+    return count
